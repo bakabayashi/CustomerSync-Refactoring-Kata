22b2fa107a26c370cec1e2b5ece7c4d5a2d2b213
rszczykutowicz <r.szczykutowicz.extern@senec.com>
2024-03-30 09:49:09 +0100

    Temporarly moved logic of loading and synchronizing customer to one place (CustomerDataAccess) in order to find out what's going on.

--- java/src/main/java/codingdojo/CustomerDataAccess.java
+++ java/src/main/java/codingdojo/CustomerDataAccess.java
@@ -8,30 +8,65 @@ public class CustomerDataAccess {
         this.customerDataLayer = customerDataLayer;
     }
 
-    public CustomerMatches loadCompanyCustomer(String externalId, String companyNumber) {
+    public CustomerMatches loadCompanyCustomer(ExternalCustomer externalCustomer) {
+        final String externalId = externalCustomer.getExternalId();
+        final String companyNumber = externalCustomer.getCompanyNumber();
         CustomerMatches matches = new CustomerMatches();
         Customer matchByExternalId = this.customerDataLayer.findByExternalId(externalId);
         if (matchByExternalId != null) {
+            verify(matchByExternalId, externalId, CustomerType.COMPANY);
             matches.setCustomer(matchByExternalId);
-            matches.setMatchTerm("ExternalId");
             Customer matchByMasterId = this.customerDataLayer.findByMasterExternalId(externalId);
-            if (matchByMasterId != null) matches.addDuplicate(matchByMasterId);
+            if (matchByMasterId != null)
+                matches.addDuplicate(matchByMasterId);
+
+            String customerCompanyNumber = matches.getCustomer().getCompanyNumber();
+            if (!companyNumber.equals(customerCompanyNumber)) {
+                matches.getCustomer().setMasterExternalId(null);
+                matches.addDuplicate(matches.getCustomer());
+                matches.setCustomer(null);
+                matches.setMatchTerm(null);
+            }
         } else {
             Customer matchByCompanyNumber = this.customerDataLayer.findByCompanyNumber(companyNumber);
+            verify(matchByCompanyNumber, externalId, CustomerType.COMPANY);
             if (matchByCompanyNumber != null) {
                 matches.setCustomer(matchByCompanyNumber);
-                matches.setMatchTerm("CompanyNumber");
+                String customerExternalId = matches.getCustomer().getExternalId();
+                if (customerExternalId != null && !externalId.equals(customerExternalId)) {
+                    throw new ConflictException("Existing customer for externalCustomer " + companyNumber + " doesn't match external id " + externalId + " instead found "
+                            + customerExternalId);
+                }
+                Customer customer = matches.getCustomer();
+                customer.setExternalId(externalId);
+                customer.setMasterExternalId(externalId);
+                matches.addDuplicate(null);
             }
         }
 
         return matches;
     }
 
-    public CustomerMatches loadPersonCustomer(String externalId) {
+    private void verify(Customer customer, String externalId, CustomerType customerType) {
+        if (customer != null && !(customerType == customer.getCustomerType())) {
+            throw new ConflictException("Existing customer for externalCustomer " + externalId + " already exists and is not a company");
+        }
+    }
+
+    public CustomerMatches loadPersonCustomer(ExternalCustomer externalCustomer) {
+        final String externalId = externalCustomer.getExternalId();
         CustomerMatches matches = new CustomerMatches();
         Customer matchByPersonalNumber = this.customerDataLayer.findByExternalId(externalId);
+        verify(matchByPersonalNumber, externalId, CustomerType.PERSON);
         matches.setCustomer(matchByPersonalNumber);
-        if (matchByPersonalNumber != null) matches.setMatchTerm("ExternalId");
+        if (matchByPersonalNumber == null) {
+            if (matches.getCustomer() != null) {
+                Customer customer = matches.getCustomer();
+                customer.setExternalId(externalId);
+                customer.setMasterExternalId(externalId);
+            }
+        }
+
         return matches;
     }
 
